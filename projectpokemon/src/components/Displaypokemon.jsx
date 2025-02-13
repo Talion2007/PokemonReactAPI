fff76497adcbfe7b9711bf4ccdf21200251cfa4d
Felipe Cagnin <cagnin.lima.2007@gmail.com>
2025-02-13 14:30:30 -0300

    V1.04

--- projectpokemon/src/components/Displaypokemon.jsx
+++ projectpokemon/src/components/Displaypokemon.jsx
@@ -4,7 +4,18 @@ function DisplayPokemon ( {pokemonData} ) {
         <>
         <section className="display-pokemon">
         <h1>{pokemonData.name}</h1>
+        <div>
         <img src={pokemonData.sprites?.front_default} alt={pokemonData.name} />
+        <img src={pokemonData.sprites?.back_default} alt={pokemonData.name} />
+        </div>
+
+        <article className="pokemon-info">
+            <p>Height: {pokemonData.height}</p>
+            <p>Weight: {pokemonData.weight}</p>
+            <p>Base Experience: {pokemonData.base_experience}</p>
+            <p>Abilities: {pokemonData.abilities?.map(ability => ability.ability.name).join(", ")}</p>
+        </article>
+
         </section>
         </>
     )
