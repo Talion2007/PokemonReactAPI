2b1094e184d1f9151ea0494ad8d2571dd7fea774
Felipe Cagnin <cagnin.lima.2007@gmail.com>
2025-02-13 13:35:36 -0300

    V1.03

--- projectpokemon/src/App.jsx
+++ projectpokemon/src/App.jsx
@@ -1,46 +1,43 @@
-import { useState, useRef } from 'react';
-import { Analytics } from "@vercel/analytics/react"
-import axios from 'axios'
+import { useState, useRef } from "react";
+import DisplayPokemon from "./components/Displaypokemon";
+import axios from "axios";
 
-import './App.css'
+import Logo from "./assets/logo.png";
 
-import DisplayPokemon from './components/Displaypokemon';
+import "./App.css";
 
 function App() {
+  const inputRef = useRef();
 
-    const inputRef = useRef();
+  const [pokemonData, setPokemonData] = useState({});
 
-    const [pokemonData, setPokemonData] = useState({})
+  async function searchPokemon() {
+    console.log(inputRef.current.value);
 
-    async function searchPokemon() {
-        console.log(inputRef.current.value)
-        
-        const pokemon = inputRef.current.value.toLowerCase();
+    const pokemon = inputRef.current.value.toLowerCase();
 
-        const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
 
-        const data = await axios.get(url)
+    const data = await axios.get(url);
 
-        setPokemonData(data.data)
+    setPokemonData(data.data);
 
-    console.log(data)
-
-    }
+    console.log(data);
+  }
 
   return (
     <>
-    <Analytics/>
-    <header className='header'>
-        <input ref={inputRef} type='text' placeholder='Search' />
-        <button onClick={searchPokemon}>Search</button>
+      <header className="header">
+        <img src= {Logo} ></img>
+        <div>
+          <input ref={inputRef} type="text" placeholder="Pokemon Name or ID" />
+          <button onClick={searchPokemon}>Search</button>
+        </div>
       </header>
 
-      <section>
-        <DisplayPokemon pokemonData={pokemonData}/>
-      </section>
- 
+        <DisplayPokemon pokemonData={pokemonData} />
     </>
-  )
+  );
 }
 
 export default App;
--- projectpokemon/src/components/Displaypokemon.jsx
+++ projectpokemon/src/components/Displaypokemon.jsx
@@ -1,8 +1,11 @@
+import "../styles/DisplayPokemon.css";
 function DisplayPokemon ( {pokemonData} ) {
     return (
         <>
+        <section className="display-pokemon">
         <h1>{pokemonData.name}</h1>
-        <img src={pokemonData.sprites.front_default} alt={pokemonData.name} />
+        <img src={pokemonData.sprites?.front_default} alt={pokemonData.name} />
+        </section>
         </>
     )
 }
