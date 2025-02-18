63203c2f86cd317332d6335029c00f1293116997
Talion Elessar <cagnin.lima.2007@gmail.com>
2025-02-18 14:22:07 -0300

    V3.01

--- projectpokemon/src/App.jsx
+++ projectpokemon/src/App.jsx
@@ -1,67 +1,59 @@
 import { useState, useRef, useEffect } from "react";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/react"
-
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import axios from "axios";
-
 import DisplayPokemon from "./components/Displaypokemon";
 import Logo from "./assets/logo.png";
 import "./App.css";
 
 function App() {
   const inputRef = useRef();
-
   const [pokemonData, setPokemonData] = useState({});
-  const [erro, setErro] = useState()
+  const [erro, setErro] = useState();
+
+  useEffect(() => {
+    searchPokemon();
+  }, []);
 
   async function searchPokemon() {
     console.log(inputRef.current.value);
-
     const pokemon = inputRef.current.value.toLowerCase();
-
     const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
-
     try {
-
       const data = await axios.get(url);
-
       if (data.status !== 404) {
-        setErro("OK")
+        setErro("OK");
       }
-
-setPokemonData(data.data);
-console.log(data.data);
-
-    } catch(error) {
-      console.log("Fetch Status:",error) 
-      setErro("Error")
+      setPokemonData(data.data);
+    } catch (error) {
+      console.log("Fetch Status:", error);
+      setErro("Error");
     }
   }
-
-useEffect (() => {
-  searchPokemon();
-} ,[])
 
   return (
     <>
       <header className="header">
-        <img src= {Logo} ></img>
+        <img src={Logo}></img>
         <div>
-          <input ref={inputRef} type="text" placeholder="Pokemon Name or ID" defaultValue={"Bulbasaur"} onKeyDown={(event) => {
-            if (event.key === "Enter") {
-              searchPokemon();
-            }
-          }}/>
+          <input
+            ref={inputRef}
+            type="text"
+            placeholder="Pokemon Name or ID"
+            defaultValue={"Bulbasaur"}
+            onKeyDown={(event) => {
+              if (event.key === "Enter") {
+                searchPokemon();
+              }
+            }}
+          />
           <button onClick={searchPokemon}>Search</button>
         </div>
       </header>
-
-        <DisplayPokemon pokemonData={pokemonData} error ={erro}/>
-
-        <Analytics/>
-        <SpeedInsights/>
+      <DisplayPokemon pokemonData={pokemonData} error={erro} />
+      <Analytics />
+      <SpeedInsights />
     </>
   );
 }
-
 export default App;
--- projectpokemon/src/components/Displaypokemon.jsx
+++ projectpokemon/src/components/Displaypokemon.jsx
@@ -4,24 +4,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/DisplayPokemon.css";
 
 function DisplayPokemon({ pokemonData, error }) {
+  const namePokemon =
+  pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1);
   if (!pokemonData || !pokemonData.name)
     return (
       <section className="display-pokemon error">
         <h1>Pokemon não encontrado...</h1>
       </section>
     );
-
-    if (error !== "OK") {
-      return (
-        <section className="display-pokemon error">
-          <h1>Pokemon não encontrado...</h1>
-        </section>
-      );
-    }
-
-  const namePokemon =
-    pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1);
-
+  if (error !== "OK") {
+    return (
+      <section className="display-pokemon error">
+        <h1>Pokemon não encontrado...</h1>
+      </section>
+    );
+  }
   return (
     <>
       <section className="display-pokemon">
@@ -51,26 +48,13 @@ function DisplayPokemon({ pokemonData, error }) {
             </Carousel.Item>
           </Carousel>
         </div>
-
         <article className="pokemon-info">
-          <p>
-            Health Points: {pokemonData.stats[0].base_stat}
-          </p>
-          <p>
-            Attack: {pokemonData.stats[1].base_stat}
-          </p>
-          <p>
-            Defense: {pokemonData.stats[2].base_stat}
-          </p>
-          <p>
-            Special Attack: {pokemonData.stats[3].base_stat}
-          </p>
-          <p>
-            Special Defense: {pokemonData.stats[4].base_stat}
-          </p>
-          <p>
-            Speed: {pokemonData.stats[5].base_stat}
-          </p>
+          <p>Health Points: {pokemonData.stats[0].base_stat}</p>
+          <p>Attack: {pokemonData.stats[1].base_stat}</p>
+          <p>Defense: {pokemonData.stats[2].base_stat}</p>
+          <p>Special Attack: {pokemonData.stats[3].base_stat}</p>
+          <p>Special Defense: {pokemonData.stats[4].base_stat}</p>
+          <p>Speed: {pokemonData.stats[5].base_stat}</p>
           <p>Base Experience: {pokemonData.base_experience}</p>
           <p>Height: {pokemonData.height / 10} meters</p>
           <p>Weight: {pokemonData.weight / 10} kilograms</p>
@@ -91,5 +75,4 @@ function DisplayPokemon({ pokemonData, error }) {
     </>
   );
 }
-
 export default DisplayPokemon;
